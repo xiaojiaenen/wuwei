e2a55e42660a2f0ad6076bd95d7944eb2732e202
xiaojiaenen <1163218095@qq.com>
2026-06-02 21:24:07 +0800

    移除 SkillViewerTool 导入和 __all__ 条目

--- wuwei/skill/__init__.py
+++ wuwei/skill/__init__.py
@@ -1,11 +1,9 @@
 from wuwei.skill.fs_provider import FileSystemSkillProvider
 from wuwei.skill.skill import Skill, SkillManager, SkillProvider
-from wuwei.skill.viewer import SkillViewerTool
 
 __all__ = [
     "Skill",
     "SkillManager",
     "SkillProvider",
     "FileSystemSkillProvider",
-    "SkillViewerTool",
 ]
